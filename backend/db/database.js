938757e5caaed09e792041b46abf384b028385d7
SilvaCastillo <60104481+SilvaCastillo@users.noreply.github.com>
2023-09-08 17:25:15 +0200

    Switched to MongoDB database
    - Refactored code to use MongoDB as the primary database.
    - Added MongoDB Atlas cloud database URI with username and password to the .env file.

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,19 +1,22 @@
-const sqlite3 = require('sqlite3').verbose();
-const { open } = require('sqlite');
+require('dotenv').config()
 
 const database = {
     openDb: async function openDb() {
-        let dbFilename = `./db/trains.sqlite`;
+        const uri = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.hkfbt.mongodb.net/folinodocs?retryWrites=true&w=majority`;
 
-        if (process.env.NODE_ENV === 'test') {
-            dbFilename = "./db/test.sqlite";
+        try {
+            const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+            await client.connect();
+            console.log('Connected to MongoDB');
+
+            // You can return the MongoDB client or any other relevant MongoDB-related object here.
+            return client;
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+            throw error;
         }
-
-        return await open({
-            filename: dbFilename,
-            driver: sqlite3.Database
-        });
-    }
+    },
 };
 
 module.exports = database;
