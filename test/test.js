98319ce5200b70a3c334a82ce43b79e6af500779
Markus Lindgren <mavn22@bth.se>
2023-09-25 16:28:07 +0200

    Added tests for all routes

--- test/test.js
+++ test/test.js
@@ -9,7 +9,12 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const server = require("../backend/app.js");
+const fetchTrainPositions = require("../backend/models/trains.js");
+const app = require("../backend/app.js");
+const codes = require('../backend/routes/codes.js');
+
+const server = app[0];
+const io = app[1];
 
 chai.should();
 
@@ -18,18 +23,50 @@ chai.use(chaiHttp);
 var assert = require("assert");
 
 describe('Reports', () => {
-    describe('GET app response', () => {
-        it('200 HAPPY PATH', (done) => {
+    describe('GET responses', () => {
+        it('/ 200 STATUS', (done) => {
             chai.request(server)
                 .get("/")
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
+                    res.body.data.should.be.an("string");
                     res.body.data.length.should.be.above(0);
 
                     done();
                 });
+        });
+        it('/codes 200 STATUS', (done) => {
+            chai.request(server)
+                .get("/codes")
+                .end((err, res) => {
+                    res.should.have.status(200);
+
+                    done();
+                });
+        });
+        it('/delayed 200 STATUS', (done) => {
+            chai.request(server)
+                .get("/codes")
+                .end((err, res) => {
+                    res.should.have.status(200);
+
+                    done();
+                });
+        });
+        it('/tickets 200 STATUS', (done) => {
+            chai.request(server)
+                .get("/codes")
+                .end((err, res) => {
+                    res.should.have.status(200);
+
+                    done();
+                });
+        });
+    });
+    describe('GET API response trains', () => {
+        it("Creates promise", () => {
+            chai.expect(fetchTrainPositions(io)).to.be.an('promise');
         });
     });
 });
