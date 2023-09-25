df05e793b4f7a584c65d1054b3e8bea9fdc09338
Markus Lindgren <mavn22@bth.se>
2023-09-25 07:44:46 +0200

    server as const variable and exported for test access

--- backend/app.js
+++ backend/app.js
@@ -40,8 +40,10 @@ app.use("/delayed", delayed);
 app.use("/tickets", tickets);
 app.use("/codes", codes);
 
-httpServer.listen(port, () => {
+const server = httpServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 fetchTrainPositions(io);
+
+module.exports = server;
