10c153b12c09760d247b736e080543d65689b758
Markus Lindgren <mavn22@bth.se>
2023-09-25 17:10:14 +0200

    route tests now also check that body is an object

--- test/test.js
+++ test/test.js
@@ -41,6 +41,7 @@ describe('Reports', () => {
                 .get("/codes")
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.body.should.be.an("object");
 
                     done();
                 });
@@ -50,6 +51,7 @@ describe('Reports', () => {
                 .get("/codes")
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.body.should.be.an("object");
 
                     done();
                 });
@@ -59,6 +61,7 @@ describe('Reports', () => {
                 .get("/codes")
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.body.should.be.an("object");
 
                     done();
                 });
